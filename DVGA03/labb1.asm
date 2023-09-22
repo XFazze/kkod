0e7b21f73d88b9439a187b8f8942fd47d83c6137
xfazze <blixtvinge@gmail.com>
2023-09-22 12:48:54 +0200

    Updated DVGA03 labb 1

--- DVGA03/labb1.asm
+++ DVGA03/labb1.asm
@@ -1,109 +1,111 @@
   .data
 antal: .word 10
 vek: .word 4,5,2,2,1,6,7,9,5,10
-#vek: .word 1,5,9,5
 
 newline: .asciiz "\n"
 space: .asciiz " "
-  .text
-  
+ 
+.text 
 main:
-  #addi $a0, $zero, 1
-  la $a2, vek
-  lw $a1, antal
+    la $a0, vek
+    lw $a1, antal
   jal skriv
   
-  la $a0, vek
-  addi $a1, $zero, 0
-  lw $a2, antal
-  sub $a2, $a2, 1
+  la $a0, vek  # vek
+  addi $a1, $zero, 0 # a
+  lw $a2, antal # antal
+  addi $a2, $a2, -1 # b = antal - 1
   jal quickSort
   
   la $a2, vek
   lw $a1, antal
   jal skriv
-  #addi $a0, $zero, 3
   j end
-	
+    
 
 skriv:
+    la $t2, ($a0)
   la $a0, newline
-  li $v0, 4
+  li $v0, 4 # syscall code
   syscall
   li $t0, 0
   b skrivLoop
-# void skriv(int vek[], int size)
 
-skrivLoop:
-  # print newline + print number+space
-  bge $t0, $a1, skrivEnd
-  sll $t1, $t0, 2 
-  add $t1, $a2, $t1
-  lw  $a0, 0($t1)
+skrivLoop: # print newline + print number + space for all numbers in vek
+  bge $t0, $a1, skrivEnd # branch if t0 >= a1(antal)
+  sll $t1, $t0, 2 # index to vector
+  add $t1, $t2, $t1
+  lw  $a0, 0($t1) # first element in that index
   addi $v0, $zero, 1
   syscall
   
+  # Print space
   la $a0, space
   li $v0, 4
   syscall
   
-  addi $t0, $t0, 1
+  addi $t0, $t0, 1 # t0++
   b skrivLoop
   
-   #addi $a0, $zero, 2
 skrivEnd:
   la $a0, newline
   li $v0, 4
   syscall
   jr $ra
 
+
 # int partition(int vek[], int a, int b)
 partition:
-  # grovsorterar arrayena?
-  # pivot=t0, lower=t1, upper=t2, temp=t3, 
-  sll $t4, $a1, 2
-  add $t4, $a0, $t4
-  lw $t0, 0($t4)
-  addi $t1, $a1, 1
-  addi $t2, $a2, 0
+  # grovsorterar arrayena
+  # t0=pivot
+  # t1=lower
+  # t2=upper 
+  # t3=temp
+   
+  sll $t4, $a1, 2 # create index
+  add $t4, $a0, $t4 # move pointing index
+  lw $t0, 0($t4) # pivot
+  addi $t1, $a1, 1 # lower
+  move $t2, $a2 # upper
   b partitionWhileLoopLower
-  
-partitionWhileLoopLower:
-  sll $t4, $t1, 2
+
+# v[lower] <= pivot && lower <= upper
+partitionWhileLoopLower: 
+  sll $t4, $t1, 2 # create lower index
   add $t4, $a0, $t4
-  lw $t4,  0($t4)
-  sle $t6,$t4, $t0 
-  sle $t7, $t1, $t2 
+  lw $t4,  0($t4) # number at lower index
+  sle $t6,$t4, $t0 # t6 = 1 if lower number <= pivot
+  sle $t7, $t1, $t2 # t7 = 1 if lower <= upper
   add $t6, $t6, $t7
-  bne $t6, 2, partitionWhileLoopUpper
-  addi $t1, $t1, 1
+  bne $t6, 2, partitionWhileLoopUpper # branch if both are true
+  addi $t1, $t1, 1 # lower++
   b partitionWhileLoopLower
   
 partitionWhileLoopUpper:
-  sll $t4,$t2, 2
+  sll $t4,$t2, 2 # create upper index
   add $t4, $a0, $t4
-  lw $t4, 0($t4)
-  sgt $t6, $t4, $t0  
-  sle $t7, $t1, $t2 
+  lw $t4, 0($t4) # number at upper index
+  sgt $t6, $t4, $t0  # t6 = 1 if upper number > pivot
+  sle $t7, $t1, $t2 # t7 = 1 if lower <= upper
   add $t6, $t6, $t7
-  bne $t6, 2, partitionWhileLoopIf
-  addi $t2, $t2, -1
+  bne $t6, 2, partitionWhileLoopIf # branch if both are true
+  addi $t2, $t2, -1 # upper--
   b partitionWhileLoopUpper
   
 partitionWhileLoopIf:
-  bgt $t1, $t2, partitionWhileEnd
-  sll $t4, $t1, 2
+  bgt $t1, $t2, partitionWhileEnd # branch if lower > upper
+  sll $t4, $t1, 2 # create lower index
   add $t4, $a0, $t4
   lw $t3, 0($t4)   # temp=v[lower]
   
-  sll $t5, $t2, 2
-  add $t5, $a0, $t5
-  lw $t6, 0($t5) 
-  sw $t6, 0($t4)   # v[lower] = v[upper]
-  sw $t3, 0($t5)
-  addi $t5, $t3, 0
-  addi $t1, $t1, 1
-  subi $t2, $t2, 1
+  sll $t5, $t2, 2 # create upper index
+  add $t5, $a0, $t5 # upper number
+  lw $t6, 0($t5) # t6 = v[upper]
+  sw $t6, 0($t4) # v[lower] = v[upper]
+  sw $t3, 0($t5) # v[upper] = temp
+  move $t5, $t3 # t5 = temp
+  addi $t1, $t1, 1 # lower++
+  addi $t2, $t2, -1 # upper--
   b partitionWhileEnd
   
 partitionWhileEnd:
@@ -116,34 +118,35 @@ partitionWhileEnd:
   lw $t6, 0($t5) # v[a]
   sw $t6, 0($t4) # v[upper] = v[a]
   sw $t3, 0($t5) # v[a] = temp
-  addi $v0, $t2,0
+  move  $v0, $t2  # k 
   
   jr $ra
   
 
-# void quicksort(int vek[], int a, int b)
+# void quicksort(int vek[], int a, int b), recursive
 quickSort:
-  # rekursiv sorterar arrayen
-  blt $a2, $a1, quickSortEnd  
+    # $a0 = vek
+    # $a1 = a = 0
+    # $a2 = b = antal-1
 
-  addi $sp, $sp, -12
-  sw $ra, 0($sp) 
-  sw $a1, 4($sp) 
-  sw $a2, 8($sp) 
+  blt $a2, $a1, quickSortEnd # branch if b < a
+
+  addi $sp, $sp, -8 # space for 2 items
+  sw $ra, 0($sp) # save return adress
+  sw $a2, 4($sp) # save original b
   
   jal partition
   
-  addi $a2, $v0, -1
+  addi $a2, $v0, -1 # k - 1
   jal quickSort
     
-  lw $a2, 8($sp) 
-  addi $a1, $v0, 1  
+  lw $a2, 4($sp) # restore original b
+  addi $a1, $v0, 1 # k + 1  
   jal quickSort
   
-  lw $ra, 0($sp) 
-  lw $a1, 4($sp) 
-  lw $a2, 8($sp) 
-  addi $sp, $sp, 12
+  lw $ra, 0($sp) # restore return adress
+  addi $sp, $sp, 8 # return stack pointer
+  
   b quickSortEnd
   
 quickSortEnd:
