f28140882d0664ae7a30b2a7fdec74ca35878170
xfazze <blixtvinge@gmail.com>
2023-09-29 01:55:24 +0200

    DVGB01 labb 3

--- DVGB01/labb3/main.c
+++ DVGB01/labb3/main.c
@@ -16,9 +16,80 @@
 
 typedef struct Logical_memory
 {
-    char address[6];
+    char address[4];
     int page;
 } Logical_memory;
+void lru_access_order(int *access_order, int access_index, int order_max_index, int reused)
+{
+    int passed_reused_value = 0;
+    // printf("lru accers called max:%d\n", order_max_index);
+    for (int i = order_max_index; i > 0; i--)
+    {
+        if (reused == 0 || passed_reused_value)
+        {
+            access_order[i] = access_order[i - 1];
+        }
+        else if (access_order[i - 1] == access_index)
+        {
+
+            passed_reused_value = 1;
+        }
+    }
+    access_order[0] = access_index;
+    // for (int i = 0; i < order_max_index; i++)
+    // {
+    //     printf("%d ", access_order[i]);
+    // }
+    // printf("\n");
+
+    // printf("moved %d to %d # %d to %d\n", access_order[i - 1], access_order[i], i - 1, i);
+}
+int optimal_find_unique(int *order, int *used_next, int frames_number)
+{
+    int least_used_page = 0;
+    for (int i = 0; i < frames_number; i++)
+    {
+        int found_unique = 1;
+        for (int o = 0; o < frames_number; o++)
+        {
+            if (order[i] == used_next[o])
+            {
+                found_unique = 0;
+            }
+        }
+        if (found_unique)
+        {
+            // printf("Found not used value = %d\n", order[i]);
+            return order[i];
+        }
+    }
+    // printf("Found last used value = %d\n", used_next[frames_number]);
+    return used_next[frames_number];
+}
+void optimal_find_next_used(int frames_number, int start_index, int *used_next, Logical_memory *logical_memory)
+{
+    int used_next_index = 0;
+    for (int i = start_index + 1; i < frames_number; i++)
+    {
+        int used_next_duplicate = 0;
+        for (int o = 0; o < frames_number; o++)
+        {
+            if (used_next[o] == logical_memory[i].page)
+            {
+                used_next_duplicate = 1;
+            }
+        }
+
+        if (used_next_duplicate == 0)
+        {
+            used_next[used_next_index] = logical_memory[i].page;
+        }
+        if (used_next_index == frames_number)
+        {
+            break;
+        }
+    }
+}
 
 int main(int arc, char **argv)
 {
@@ -30,8 +101,6 @@ int main(int arc, char **argv)
     char file_buffer[MAX_PROCESS_DATA_LENGTH];
     struct Logical_memory logical_memory[MAX_LOGICAL_MEMORY_ADRESSES];
     int memory_count = 0;
-    int pages[MAX_LOGICAL_MEMORY_ADRESSES];
-    int page_count = 0;
     srand(time(NULL));
     for (int i = 0; i < arc; ++i)
     {
@@ -58,6 +127,11 @@ int main(int arc, char **argv)
         printf("You need to provide an algorithm with -a.\n");
         return 0;
     }
+    if (frames_number == 0)
+    {
+        printf("You need to provide frame number with -n.\n");
+        return 0;
+    }
 
     fp = fopen(filename, "r");
     if (fp == NULL)
@@ -70,59 +144,176 @@ int main(int arc, char **argv)
     while (fgets(file_buffer, MAX_PROCESS_DATA_LENGTH, fp))
     {
         // printf("=%s\n", file_buffer);
+        // printf("file buffer %s\n", file_buffer);
         struct Logical_memory new_logical_memory;
-        memcpy(new_logical_memory.address, file_buffer + 2, 4);
-        // printf("page %s\n", file_buffer);
-        char page[20];
-        memcpy(page, file_buffer + 3, 1);
-        // printf("pageb %s\n", page);
+        strncpy(new_logical_memory.address, file_buffer + 2, 4);
+        // printf("address %s\n", new_logical_memory.address);
 
-        new_logical_memory.page = atoi(page);
+        new_logical_memory.page = file_buffer[3] - 48;
         logical_memory[memory_count] = new_logical_memory;
-        printf("aaa address:%s page:%d  \n", logical_memory[memory_count].address, logical_memory[memory_count].page);
+        // printf("aaa address:%s page:%d  \n", logical_memory[memory_count].address, logical_memory[memory_count].page);
         memory_count++;
     }
     fclose(fp);
 
-    if (strcpy(algorithm, "fifo") == 0)
+    int pages[MAX_LOGICAL_MEMORY_ADRESSES] = {0};
+    int page_count = 0;
+    int memory_accesses = 0;
+    int page_hits = 0;
+    int page_faults = 0;
+    int page_replacements = 0;
+
+    if (strcmp(algorithm, "fifo") == 0)
     {
         int order[MAX_LOGICAL_MEMORY_ADRESSES];
         int order_max_index = 0;
         int order_queue_index = 0;
 
-        while (1)
+        for (int i = 0; i < memory_count; i++)
         {
-            for (int i = 0; i < memory_count; i++)
+            // printf("loop %d pages  \n", i);
+            printf("\n");
+            memory_accesses++;
+            if (pages[logical_memory[i].page])
             {
-                if (pages[logical_memory[i].page])
+                printf("Address %s is on page %d which is already in physical memory\n", logical_memory[i].address, logical_memory[i].page);
+                page_hits++;
+            }
+            else
+            {
+                printf("Address %s not in physical memory\n", logical_memory[i].address);
+                if (page_count < frames_number)
                 {
-                    printf("Address %s is on page %d which is already in physical memory\n", logical_memory[i].address, logical_memory[i].page);
+                    page_count++;
+                    pages[logical_memory[i].page] = 1;
+                    order[order_max_index] = logical_memory[i].page;
+                    order_max_index++;
+                    printf("Page #%d is paged in\n", logical_memory[i].page);
                 }
                 else
                 {
-                    printf("Address");
-                    if (page_count < frames_number)
+                    printf("Page #%d is paged out\n", order[order_queue_index]);
+                    pages[order[order_queue_index]] = 0;
+                    printf("Page #%d is paged in\n", logical_memory[i].page);
+                    pages[logical_memory[i].page] = 1;
+                    order[order_queue_index] = logical_memory[i].page;
+                    order_queue_index++;
+                    if (order_queue_index >= page_count)
                     {
-                        page_count++;
-                        pages[logical_memory[i].page] = 1;
-                        order[order_max_index] = logical_memory[i].page;
-                        order_max_index++;
+                        order_queue_index = 0;
                     }
-                    else
-                    {
-                        printf("Page #%d is paged out", order[order_queue_index]);
-                        printf("Page #%d is paged in", logical_memory[i].page);
-                        order[order_queue_index] = logical_memory[i].page;
-                        order_queue_index++;
-                        if (order_queue_index >= page_count)
-                        {
-                            order_queue_index = 0;
-                        }
-                    }
+                    page_replacements++;
                 }
+                page_faults++;
             }
         }
     }
+    else if (strcmp(algorithm, "lru") == 0)
+    {
+        int access_order[MAX_LOGICAL_MEMORY_ADRESSES] = {0};
+        int order_max_index = 0;
+
+        for (int i = 0; i < memory_count; i++)
+        {
+            // printf("loop %d pages  \n", i);
+            printf("\n");
+            memory_accesses++;
+            if (pages[logical_memory[i].page])
+            {
+                printf("Address %s is on page %d which is already in physical memory\n", logical_memory[i].address, logical_memory[i].page);
+                lru_access_order(access_order, logical_memory[i].page, order_max_index, 1);
+                page_hits++;
+            }
+            else
+            {
+                printf("Address %s not in physical memory\n", logical_memory[i].address);
+                if (page_count < frames_number)
+                {
+                    page_count++;
+                    pages[logical_memory[i].page] = 1;
+                    lru_access_order(access_order, logical_memory[i].page, order_max_index, 0);
+
+                    order_max_index++;
+                    printf("Page #%d is paged in\n", logical_memory[i].page);
+                }
+                else
+                {
+                    printf("Page #%d is paged out\n", access_order[page_count - 1]);
+                    pages[access_order[page_count]] = 0;
+                    printf("Page #%d is paged in\n", logical_memory[i].page);
+                    pages[logical_memory[i].page] = 1;
+                    lru_access_order(access_order, logical_memory[i].page, order_max_index, 0);
+
+                    page_replacements++;
+                }
+                page_faults++;
+            }
+        }
+    }
+    else if (strcmp(algorithm, "optimal") == 0)
+    {
+        int order[MAX_LOGICAL_MEMORY_ADRESSES];
+        int order_max_index = 0;
+        int used_next[frames_number];
+        int order_queue_index = 0;
+
+        for (int i = 0; i < memory_count; i++)
+        {
+            // printf("loop %d pages  \n", i);
+            printf("\n");
+            memory_accesses++;
+            if (pages[logical_memory[i].page])
+            {
+                printf("Address %s is on page %d which is already in physical memory\n", logical_memory[i].address, logical_memory[i].page);
+                page_hits++;
+            }
+            else
+            {
+                printf("Address %s not in physical memory\n", logical_memory[i].address);
+                if (page_count < frames_number)
+                {
+                    page_count++;
+                    pages[logical_memory[i].page] = 1;
+
+                    order[order_max_index] = logical_memory[i].page;
+                    order_max_index++;
+
+                    order_max_index++;
+                    printf("Page #%d is paged in\n", logical_memory[i].page);
+                }
+                else
+                {
+                    memset(used_next, 0, frames_number * sizeof(int));
+                    optimal_find_next_used(frames_number, i, used_next, logical_memory);
+                    int replace_page = optimal_find_unique(order, used_next, frames_number);
+
+                    printf("Page #%d is paged out\n", replace_page);
+                    pages[replace_page] = 0;
+                    printf("Page #%d is paged in\n", logical_memory[i].page);
+                    pages[logical_memory[i].page] = 1;
+                    for (int o = 0; o < frames_number; o++)
+                    {
+                        if (replace_page == order[o])
+                        {
+                            // printf("reaplcae in orddr %d with %d", order[o], logical_memory[i].page);
+                            order[o] = logical_memory[i].page;
+                            break;
+                        }
+                    }
+
+                    page_replacements++;
+                }
+                page_faults++;
+            }
+        }
+    }
+    printf("\nRunning simulation...Done.\n\nSimulation Summary\n\n");
+    printf("%-20s %s\n", "Algorithm:", algorithm);
+    printf("%-20s %d\n", "Frames:", frames_number);
+    printf("%-20s %d\n", "Memeory accesses:", memory_accesses);
+    printf("%-20s %d\n", "Page hits:", page_hits);
+    printf("%-20s %d\n", "Page faults:", page_faults);
+    printf("%-20s %d\n", "Page replacements:", page_replacements);
 
     return 0;
 }
