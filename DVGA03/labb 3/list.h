dac4db3d547c0ea17eb56fa641d076913bd9001c
xfazze <blixtvinge@gmail.com>
2023-10-13 13:38:23 +0200

    labb 3 doe

--- DVGA03/labb 3/list.h
+++ DVGA03/labb 3/list.h
@@ -3,35 +3,37 @@
 #define LIST
 
 // Calculerar storleken på listan. Antalet noder.
-int LSize(node* head);
+int LSize(node *head);
 
 // Kontrollerar ifall headan är tom. Returnerar 1 för tom och 0 för icke-tom.
-int LIsEmpty(node* head);
+int LIsEmpty(node *head);
 
-// Returnerar ett element från ett visst index i headan. Returnerar -1 ifall elementet inte finns.
-node* LGetNode(node* head, int index);
+// Returnerar en node från ett visst index i listan. Noden måste finnas.
+node *LGetNode(node *head, int index);
 
 // Returnerar 1 ifall värdet finns i headan annars 0.
-int LContains(node* head, int value);
+int LContains(node *head, int value);
 
 // Returnerar indexet vid första förekomsten av ett värde. Returnerar -1 ifall elementet inte finns.
-int LGetFirstIndexOf(node* head, int value);
+int LGetFirstIndexOf(node *head, int value);
 
 // Lägger till ett värde på en viss plats i headan. Index måste vara lika eller mindre än storleken på listan.
-node* LInsertElement(node* head, int value, int index);
+// Returnerar head.
+node *LInsertElement(node *head, int value, int index);
 
 // Tar bort ett element på en viss plats i headan. Index måste vara mindre än storleken på listan.
-node* LRemoveAtIndex(node* head, int index);
+// Returnerar head.
+node *LRemoveAtIndex(node *head, int index);
 
-// Sorterar i stigande ordning. Listans storlek måste vara minst 2.
-node* LSort(node* head);
+// Sorterar i stigande ordning. Listans storlek måste vara minst 2. Returnerar head.
+node *LSort(node *head);
 
-// Returnerar nästa node. Nästa node måste finnas.
-node* LGetNext(node* cnode);
+// Returnerar nästa node. Noden och nästa node måste finnas. Returnerar head.
+node *LGetNext(node *cnode);
 
-// Returnerar väret på noden. Noden måste ha ett värde.
-int LGetValue(node* cnode);
+// Returnerar väret på noden. Noden måste finnsa och ha ett värde.
+int LGetValue(node *cnode);
 
-// Rekursiv funktion som vänder på listan. Listan måste innehålla minst ett värde.
-node* LReverse(node* cnode, int depth);
+// Rekursiv funktion som vänder på listan. Listan måste innehålla minst ett värde. Returnerar head.
+node *LReverse(node *cnode, int depth);
 #endif
